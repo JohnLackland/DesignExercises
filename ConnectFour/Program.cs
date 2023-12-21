b4e6c480bbab7a139cc75bfba87d82a5e79df1ca
John Lackland <frksjns96@gmail.com>
2023-12-21 11:19:40 +0100

    Add program.cs

---
+++ ConnectFour/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConnectFour.Enums;
+using ConnectFour.Models;
+
+namespace DesignExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, the game will start soon...");
+            var player1 = new Player("Player 1", CellState.MArkedWithO);
+            var player2 = new Player("Player 2 ", CellState.MarkedWithX);
+            var game = new Game([player1, player2], 2);
+            game.Play();
+        }
+    }
+}
